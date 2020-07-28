f13b686069d55e66fcbd36c3bcdf5204b605c7e8
salvipragnesh <61496853+salvipragnesh@users.noreply.github.com>
2020-07-28 19:47:20 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>PHP</title>
+</head>
+<body>
+<form action="getpost.php" method="post">
+
+<input type="text" name="firstname" placeholder="First Name" />
+
+<input type="text" name="lastname" placeholder="Last Name" />
+
+<input type="submit" name="submit" />
+</form>
+</body>
+</html>
+
+<?php
+if(isset($_POST['firstname']))
+{
+$data = $_POST['firstname'];
+$file1 = fopen('newfile.txt','a');
+fwrite($file1,$data);
+fclose($file1); 
+}
+?>
